afe4af14048830bca61472ceafdac720a8e475e5
Zaid Hassan <zaidhassan251@gmail.com>
2025-06-11 21:09:29 +0530

    Update Hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { motion } from "framer-motion";
-import TiltedCard from "./components/tiltedCard/TiltedCard";
 import ClickSpark from "./components/clickSpark/ClickSpark";
-import Hero from "./components/home/Home";
 import Mentors from "./components/mentors/Mentors";
 import OurOfferings from "./components/exploreZiit/ExploreZiit";
 import Partners from "./components/partners/Partners";
 import Pricing from "./components/pricing/Pricing";
 import Navbar from "./components/navbar/Navbar";
-import { Home } from "lucide-react";
+import Home from "./components/home/Home";
 
 function App() {
   return (
@@ -22,20 +19,7 @@ function App() {
         <div className="fixed top-0 left-0 w-full h-fit z-50">
           <Navbar />
         </div>
-        {/* Hero Section */}
         <Home />
-
-        {/* Our offerings */}
-        <OurOfferings />
-
-        {/* Pricing */}
-        <Pricing />
-
-        {/* Mentors Section */}
-        <Mentors />
-
-        {/* Partners */}
-        <Partners />
       </ClickSpark>
     </section>
   );
